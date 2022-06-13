c02f09f5f175d169791015e836748c2b4a5140ff
Kristoffer Larberg <mail@kristofferlarberg.se>
2022-06-13 15:34:07 +0200

    fix: add missing Formik related structure

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -15,25 +15,30 @@ const Search = () => {
     date: '',
   };
 
-  const handleSubmit = () => {
-    console.log('submitting form');
-  };
-
   return (
     <>
       <h1 className="mb-10">Sök resa</h1>
-      <Formik initialValues={initialFormState} onSubmit={handleSubmit}>
-        <div className="items-left flex w-full max-w-md flex-col gap-10">
-          <label htmlFor="from">Från</label>
-          <Field name="from" placeholder="Välj start" />
-          <label htmlFor="to">Till</label>
-          <Field name="to" placeholder="Välj mål" />
-          <label htmlFor="date">Datum</label>
-          <Field type="date" name="date" />
-          <label htmlFor="time">Time</label>
-          <Field type="time" />
-          <button type="submit">Sök</button>
-        </div>
+      <Formik
+        initialValues={initialFormState}
+        onSubmit={() => console.log('submitting form')}
+      >
+        {({ handleSubmit }) => (
+          <form
+            method="get"
+            onSubmit={handleSubmit}
+            className="items-left flex w-full max-w-md flex-col gap-10"
+          >
+            <label htmlFor="from">Från</label>
+            <Field name="from" placeholder="Välj start" />
+            <label htmlFor="to">Till</label>
+            <Field name="to" placeholder="Välj mål" />
+            <label htmlFor="date">Datum</label>
+            <Field name="date" type="date" />
+            <label htmlFor="time">Time</label>
+            <Field name="time" type="time" />
+            <button type="submit">Sök</button>
+          </form>
+        )}
       </Formik>
     </>
   );
