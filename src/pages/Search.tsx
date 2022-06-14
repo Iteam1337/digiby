dc833dc3bfb86cb989d861c74a44ac58eecfc995
Kristoffer Larberg <mail@kristofferlarberg.se>
2022-06-14 15:08:12 +0200

    feat: add form error handling

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,18 +1,53 @@
 import { useState } from 'react';
-import { Formik, Field } from 'formik';
+import {
+  Formik,
+  Field,
+  ErrorMessage,
+  useField,
+  useFormikContext,
+} from 'formik';
 import DatePicker from 'react-datepicker';
 import moment from 'moment';
 import 'react-datepicker/dist/react-datepicker.css';
+import * as Yup from 'yup';
+
+export const DatePickerField = ({
+  name,
+  startDate,
+  setStartDate,
+}: {
+  name: string;
+  startDate: Date;
+  setStartDate: (date: Date) => void;
+}) => {
+  const { setFieldValue, setFieldTouched } = useFormikContext();
+  const [field] = useField(name);
+  return (
+    <DatePicker
+      name={name}
+      selected={startDate}
+      onChange={(date: Date) => {
+        setFieldValue(field.name, date);
+        setStartDate(date);
+      }}
+      onChangeRaw={() => {
+        setFieldTouched(field.name, true, true);
+      }}
+      showTimeSelect
+      dateFormat="Pp"
+    />
+  );
+};
 
 const Search = () => {
-  const [startDate, setStartDate] = useState(new Date);
-  
+  const [startDate, setStartDate] = useState(new Date());
+
   const formatDate = (date: Date) => {
-    return moment(date).format('L')
-  }
+    return moment(date).format('L');
+  };
   const formatTime = (date: Date) => {
-    return moment(date).format('HH:mm')
-  }
+    return moment(date).format('HH:mm');
+  };
 
   type FormData = {
     from: string;
@@ -28,15 +63,32 @@ const Search = () => {
     date: formatDate(startDate),
   };
 
+  const SignupSchema = Yup.object().shape({
+    from: Yup.string()
+      .min(2, 'För kort')
+      .max(70, 'För långt')
+      .required('Lägg till start'),
+    to: Yup.string()
+      .min(2, 'För kort')
+      .max(70, 'För långt')
+      .required('Lägg till mål'),
+    dateAndTime: Yup.date()
+      .nullable()
+      .required('Lägg till tidpunkt')
+      .min(new Date(), 'Tidpunkt kan inte vara tidigare än dagens datum'),
+  });
+
   return (
     <>
       <h1 className="mb-10">Sök resa</h1>
       <Formik
         initialValues={initialFormState}
-        onSubmit={(formState) =>{ 
-          formState.time = formatTime(startDate)
-          formState.date = formatDate(startDate)
-          console.log('submitting form', formState)}}
+        validationSchema={SignupSchema}
+        onSubmit={(formState) => {
+          formState.time = formatTime(startDate);
+          formState.date = formatDate(startDate);
+          console.log('submitting form', formState);
+        }}
       >
         {({ handleSubmit }) => (
           <form
@@ -46,17 +98,17 @@ const Search = () => {
           >
             <label htmlFor="from">Från</label>
             <Field name="from" placeholder="Välj start" />
+            <ErrorMessage name="from" />
             <label htmlFor="to">Till</label>
             <Field name="to" placeholder="Välj mål" />
-
-          <label htmlFor='dateAndTime'>Välj när du vill åka</label>
-            <DatePicker
-              name='dateAndTime'
-              selected={startDate}
-              onChange={(date: Date) => setStartDate(date)}
-              showTimeSelect
-              dateFormat="Pp"
+            <ErrorMessage name="to" />
+            <label htmlFor="dateAndTime">Välj när du vill åka</label>
+            <DatePickerField
+              name="dateAndTime"
+              startDate={startDate}
+              setStartDate={setStartDate}
             />
+            <ErrorMessage name="dateAndTime" />
             <button type="submit">Sök</button>
           </form>
         )}
