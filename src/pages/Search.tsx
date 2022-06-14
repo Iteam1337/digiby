feb430d60111135c8a1ed968995b4207e848d66f
Mikaela Lundsgård <mikaelalundsgard@hotmail.com>
2022-06-14 09:28:34 +0200

    feat: add react-datepicker

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,18 +1,31 @@
+import { useState } from 'react';
 import { Formik, Field } from 'formik';
+import DatePicker from 'react-datepicker';
+import moment from 'moment';
+import 'react-datepicker/dist/react-datepicker.css';
 
 const Search = () => {
-  type initialFormState = {
+  const [startDate, setStartDate] = useState(new Date);
+  
+  const formatDate = (date: Date) => {
+    return moment(date).format('L')
+  }
+  const formatTime = (date: Date) => {
+    return moment(date).format('HH:mm')
+  }
+
+  type FormData = {
     from: string;
     to: string;
     time: string;
     date: string;
   };
 
-  const initialFormState: initialFormState = {
+  const initialFormState: FormData = {
     from: '',
     to: '',
-    time: '',
-    date: '',
+    time: formatTime(startDate),
+    date: formatDate(startDate),
   };
 
   return (
@@ -20,7 +33,10 @@ const Search = () => {
       <h1 className="mb-10">Sök resa</h1>
       <Formik
         initialValues={initialFormState}
-        onSubmit={() => console.log('submitting form')}
+        onSubmit={(formState) =>{ 
+          formState.time = formatTime(startDate)
+          formState.date = formatDate(startDate)
+          console.log('submitting form', formState)}}
       >
         {({ handleSubmit }) => (
           <form
@@ -32,10 +48,15 @@ const Search = () => {
             <Field name="from" placeholder="Välj start" />
             <label htmlFor="to">Till</label>
             <Field name="to" placeholder="Välj mål" />
-            <label htmlFor="date">Datum</label>
-            <Field name="date" type="date" />
-            <label htmlFor="time">Time</label>
-            <Field name="time" type="time" />
+
+          <label htmlFor='dateAndTime'>Välj när du vill åka</label>
+            <DatePicker
+              name='dateAndTime'
+              selected={startDate}
+              onChange={(date: Date) => setStartDate(date)}
+              showTimeSelect
+              dateFormat="Pp"
+            />
             <button type="submit">Sök</button>
           </form>
         )}
