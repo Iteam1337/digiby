88239aea6147893cab3d04e7efa2ce3cbdcc6d8e
Mikaela Lundsgård <mikaelalundsgard@hotmail.com>
2022-06-21 13:41:15 +0200

    style: update background color based on pathname

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,19 +2,20 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Layout from './components/Layout';
 import Search from './pages/Search';
+import Departures from './pages/Departures';
 
 function App() {
   return (
+    <BrowserRouter>
     <Layout>
-      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Search />} />
-          <Route path="/departures" element={<p>Departures</p>} />
+          <Route path="/departures" element={<Departures />} />
           <Route path="/results" element={<p>Results</p>} />
           {/* <Route component={NotFound} /> */}
         </Routes>
-      </BrowserRouter>
     </Layout>
+      </BrowserRouter>
   );
 }
 
--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,8 +1,33 @@
+import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+
 const Layout = ({ children }: { children: JSX.Element }) => {
+  const [backgroundColor, setBackgroundColor] = useState('');
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    switch (pathname) {
+      case '/':
+        setBackgroundColor('home');
+        break;
+      case '/departures':
+        setBackgroundColor('departures');
+        break;
+      default:
+        break;
+    }
+  }, [pathname]);
+
   return (
     <>
-      <header className="mb-6 p-6">Header</header>
-      <main className="mx-auto w-full max-w-screen-sm">{children}</main>
+      <div
+        className={`${
+          backgroundColor === 'home' ? 'bg-pm-green' : 'bg-pm-background'
+        } h-screen`}
+      >
+        <header className="mb-6 bg-pm-green p-6">Header</header>
+        <main className="mx-auto w-full max-w-screen-sm">{children}</main>
+      </div>
     </>
   );
 };
