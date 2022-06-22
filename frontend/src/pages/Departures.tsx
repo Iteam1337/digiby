0def08a78662cb2185dc8840f0dd3ab76c25cf9a
Kristoffer Larberg <mail@kristofferlarberg.se>
2022-06-22 17:17:15 +0200

    feat: render api data in cards

--- frontend/src/pages/Departures.tsx
+++ frontend/src/pages/Departures.tsx
@@ -1,22 +1,46 @@
+import { useQuery } from 'react-query';
+
 import DeparturesCard from '../components/DeparturesCard';
+import getTransports from '../utils/getTransports';
 
 const Departures = () => {
+  // todo: find solution to ts error below by adding generics (<..., Error>)?
+  const { isLoading, isError, data, error } = useQuery(
+    'transports',
+    getTransports
+  );
+
+  if (isLoading) {
+    return <span>Söker...</span>;
+  }
+
+  if (isError) {
+    // @ts-ignore: Unreachable code error
+    return <span>Något gick fel: {error.message}</span>;
+  }
+
   return (
-    <section className=" bg-pm-background mx-4">
-      <h3 className='my-6 text-xl font-bold'>Idag</h3>
+    <section className=" mx-4 bg-pm-background">
+      <h3 className="my-6 text-xl font-bold">Idag</h3>
       <DeparturesCard
-        vehicle="Buss 23"
-        vehicleInfo="Länstrafiken, stomlinje"
-        time="07:32 - 08:45"
-        totalTime="1h 17 min"
-        cost="153 SEK"
+        vehicle={data[0].route_id}
+        vehicleInfo={data[0].transportation_type}
+        time={`${data[0].departure.arrives_at.slice(
+          11,
+          16
+        )}–${data[0].destination.arrives_at.slice(11, 16)}`}
+        totalTime={`${data[0].travel_time} min`}
+        cost={`${data[0].cost} SEK`}
       />
       <DeparturesCard
-        vehicle="Buss 23"
-        vehicleInfo="Länstrafiken, stomlinje"
-        time="07:32 - 08:45"
-        totalTime="1h 17 min"
-        cost="153 SEK"
+        vehicle={data[0].route_id}
+        vehicleInfo={data[0].transportation_type}
+        time={`${data[0].departure.arrives_at.slice(
+          11,
+          16
+        )}–${data[0].destination.arrives_at.slice(11, 16)}`}
+        totalTime={`${data[0].travel_time} min`}
+        cost={`${data[0].cost} SEK`}
       />
     </section>
   );
