c4ba3cec396e737698eab8b6b9a46f591d0b7056
Mikael Gråborg <mikael.graborg@iteam.se>
2022-06-28 16:29:48 +0200

    feat: add more departures

--- backend/lib/digiby/linjebuss.ex
+++ backend/lib/digiby/linjebuss.ex
@@ -22,15 +22,24 @@ defmodule Digiby.Linjebuss do
       end
       |> Enum.reject(fn e -> e |> Tuple.to_list() |> Enum.any?(&is_nil/1) end)
 
-    {best_trip_id, best_start_stop, best_stop_stop} =
-      Enum.min_by(best_matches_for_lines, fn {_trip_id,
-                                              %{meters_from_query_to_stop: distance_to_pickup},
-                                              %{meters_from_query_to_stop: distance_to_dropoff}} ->
+    best_sorted_departures =
+      Enum.sort_by(best_matches_for_lines, fn {_trip_id,
+                                               %{meters_from_query_to_stop: distance_to_pickup},
+                                               %{meters_from_query_to_stop: distance_to_dropoff}} ->
         distance_to_pickup + distance_to_dropoff
       end)
+      |> Enum.take(20)
 
-    Enum.filter(buses, fn %{trip_id: id} -> best_trip_id == id end)
-    |> Enum.map(fn %{stop_times: stops} = bus ->
+    best_trip_ids =
+      best_sorted_departures |> Enum.map(fn {best_trip_id, _, _} -> best_trip_id end)
+
+    buses
+    |> Enum.filter(fn %{trip_id: id} -> id in best_trip_ids end)
+    |> Enum.map(fn %{stop_times: stops, trip_id: id} = bus ->
+      {_, best_start_stop, best_stop_stop} =
+        best_sorted_departures
+        |> Enum.find(nil, fn {best_trip_id, _, _} -> best_trip_id == id end)
+
       stops =
         Enum.drop_while(stops, fn %{stop_position: %{name: name}} ->
           name != best_start_stop[:stop_position][:name]
@@ -40,9 +49,9 @@ defmodule Digiby.Linjebuss do
         end)
         |> Enum.concat([Map.drop(best_stop_stop, [:meters_from_query_to_stop])])
 
-      %{bus | stop_times: stops}
+      {%{bus | stop_times: stops}, {best_start_stop, best_stop_stop}}
     end)
-    |> Enum.map(fn bus ->
+    |> Enum.map(fn {bus, {best_start_stop, best_stop_stop}} ->
       {first_stop, last_stop} = {List.first(bus.stop_times), List.last(bus.stop_times)}
 
       travel_time =
