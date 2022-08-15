78611c12dd240419f582f97b51aea3cf9b39f273
Kristoffer Larberg <mail@kristofferlarberg.se>
2022-08-15 16:53:27 +0200

    feat: set colors on icons individually

--- frontend/src/pages/DepartureDetails.tsx
+++ frontend/src/pages/DepartureDetails.tsx
@@ -3,25 +3,9 @@ import { useAtom } from 'jotai';
 import StaticMap from 'react-map-gl';
 import DeckGL, { GeoJsonLayer, IconLayer } from 'deck.gl';
 
-import pin from '../icons/pin.svg';
-import startPin from '../icons/startPin.svg';
 import { departuresDetails, departuresAtom } from '../utils/atoms';
 import DepartureInfo from '../components/DepartureInfo';
 import { Departure } from '../utils/types';
-
-// type Feature = {
-//   type: string;
-//   properties: { color: number[] };
-//   geometry: {
-//     type: string;
-//     coordinates: LatLng[];
-//   };
-// };
-
-// type GeoJsonObject = {
-//   type: string;
-//   features: Feature[] | undefined;
-// };
 
 const DeparturesDetails = () => {
   const [mapState, setMapState] = useState({
@@ -60,32 +44,16 @@ const DeparturesDetails = () => {
     );
   }
 
-  // const stopPosition = departure?.stops.slice(-1)[0].stop_position;
-  const stopPositions = departures.data.map((dep: Departure) => ({
-    coordinates: [
-      parseFloat(dep.stops.slice(-1)[0].stop_position.lng),
-      parseFloat(dep.stops.slice(-1)[0].stop_position.lat),
-    ],
-  }));
-
-  // const startPosition = departure.stops[0].stop_position;
-  const startPositions = departures.data.map((dep: Departure) => ({
-    coordinates: [
-      parseFloat(dep.stops[0].stop_position.lng),
-      parseFloat(dep.stops[0].stop_position.lat),
-    ],
-  }));
-
   const geoJsonObject = {
     type: 'FeatureCollection',
     features: departures.data
       ?.filter((dep) => dep.geometry.length > 0)
       .map((dep) => {
-        console.log('dep.departure.arrival_time', dep.departure.arrival_time);
         return {
           type: 'Feature',
           properties: {
             color:
+              // todo: check <id> instead of <arrival_time> when <id> endpoint is available from api.
               dep.departure.arrival_time === departure.departure.arrival_time
                 ? [19, 197, 123]
                 : [200, 200, 200],
@@ -93,16 +61,65 @@ const DeparturesDetails = () => {
           geometry: { type: 'LineString', coordinates: dep.geometry },
         };
       })
-      .sort(function (x, y) {
-        return x.properties.color == [19, 197, 123]
-          ? -1
-          : y.properties.color == [19, 197, 123]
+      .sort((a, b) => {
+        return a.properties.color[0] === 19
           ? 1
+          : b.properties.color[0] === 19
+          ? -1
           : 0;
       }),
   };
 
-  console.log('geoJsonObject', geoJsonObject);
+  const positions = departures.data
+    .map((dep: Departure) => {
+      return {
+        coordinates: {
+          from: [
+            parseFloat(dep.stops[0].stop_position.lng),
+            parseFloat(dep.stops[0].stop_position.lat),
+          ],
+          to: [
+            parseFloat(dep.stops.slice(-1)[0].stop_position.lng),
+            parseFloat(dep.stops.slice(-1)[0].stop_position.lat),
+          ],
+        },
+        // todo: check <id> instead of <arrival_time> when <id> endpoint is available from api.
+        color:
+          dep.departure.arrival_time === departure.departure.arrival_time
+            ? [
+                [19, 197, 123],
+                [255, 255, 255],
+              ]
+            : [
+                [200, 200, 200],
+                [200, 200, 200],
+              ],
+      };
+    })
+    .sort((a, b) => {
+      return a.color[0][0] === 19 ? 1 : b.color[0][0] === 19 ? -1 : 0;
+    });
+
+  positions.forEach((position) => console.log(position.color[0]));
+
+  function createStopIcon(colorArr: number[][]) {
+    return `
+      <svg width="16" height="20" viewBox="0 0 8 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M3.50625 10.9483C2.41875 9.54031 0 6.12773 0 4.2109C0 1.88525 1.79083 0 4 0C6.20833 0 8 1.88525 8 4.2109C8 6.12773 5.5625 9.54031 4.49375 10.9483C4.2375 11.2839 3.7625 11.2839 3.50625 10.9483ZM4 5.61453C4.73542 5.61453 5.33333 4.98509 5.33333 4.2109C5.33333 3.43671 4.73542 2.80726 4 2.80726C3.26458 2.80726 2.66667 3.43671 2.66667 4.2109C2.66667 4.98509 3.26458 5.61453 4 5.61453Z" fill="rgb(${colorArr[0]})"/>
+</svg>
+    `;
+  }
+
+  function createStartIcon(colorArr: number[][]) {
+    return `
+      <svg width="10" height="10" viewBox="0 0 5 5" fill="none" xmlns="http://www.w3.org/2000/svg">
+<circle cx="2.5" cy="2.5" r="2" fill="rgb(${colorArr[1]})" stroke="rgb(${colorArr[0]})"/>
+</svg>`;
+  }
+
+  function svgToDataURL(svg: string) {
+    return `data:image/svg+xml;charset=utf-8,${encodeURIComponent(svg)}`;
+  }
 
   const busLayer = new GeoJsonLayer({
     id: 'geojson-layer',
@@ -121,33 +138,31 @@ const DeparturesDetails = () => {
     getElevation: 3,
   });
 
-  console.log('stopPositions', stopPositions);
-
   const stopPositionLayer = new IconLayer({
     id: 'stop-icon-layer',
-    data: stopPositions,
-    getIcon: () => ({
-      url: pin,
+    data: positions,
+    getIcon: (d: any) => ({
+      url: svgToDataURL(createStopIcon(d.color)),
       mask: false,
       width: 16,
       height: 20,
     }),
     sizeScale: 1,
-    getPosition: (d: any) => d.coordinates,
+    getPosition: (d: any) => d.coordinates.to,
     getSize: () => 20,
   });
 
   const startPositionLayer = new IconLayer({
     id: 'icon-layer',
-    data: startPositions,
-    getIcon: () => ({
-      url: startPin,
+    data: positions,
+    getIcon: (d: any) => ({
+      url: svgToDataURL(createStartIcon(d.color)),
       mask: false,
       width: 10,
       height: 10,
     }),
     sizeScale: 1,
-    getPosition: (d: any) => d.coordinates,
+    getPosition: (d: any) => d.coordinates.from,
     getSize: () => 10,
   });
 
