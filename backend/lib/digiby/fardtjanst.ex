bb0638ba04139d6e005bd73a8a13809491f9ff60
Mikael Gråborg <mikael.graborg@iteam.se>
2022-08-16 16:21:16 +0200

    refactor(fardtjanst): make less Osrm calls and try to reduce complexity

--- backend/lib/digiby/fardtjanst.ex
+++ backend/lib/digiby/fardtjanst.ex
@@ -18,34 +18,53 @@ defmodule Digiby.Fardtjanst do
     new_date = Date.from_erl!({2019, month, day})
 
     Digiby.Adapters.Fardtjanst.get_transports(Date.to_string(new_date))
+    |> Enum.filter(fn %{"from_position" => departure, "to_position" => destination} ->
+      {query_to_lng, query_to_lat} = query_to_position
+
+      before_duration = Osrm.route([departure, destination]) |> Map.get("duration")
+
+      after_duration =
+        Osrm.route([
+          departure,
+          %{"lat" => query_to_lat, "lng" => query_to_lng},
+          destination
+        ])
+        |> Map.get("duration")
+
+      after_duration - before_duration < @max_extra_travel_time_for_fardtjanst
+    end)
     |> Enum.map(fn %{
                      "type" => type,
                      "from_position" => from_position,
                      "from_street" => from_street,
                      "departure_time" => departure_time,
-                     "to_position" => to_position,
                      "to_street" => to_street,
                      "id" => id
                    } ->
       from_stop_position = Map.put(from_position, :name, from_street)
-      to_stop_position = Map.put(to_position, :name, to_street)
+
+      {query_to_lng, query_to_lat} = query_to_position
+
+      to_stop_position =
+        Map.put(%{"lat" => query_to_lat, "lng" => query_to_lng}, :name, to_street)
 
       first_stop =
         Map.new()
         |> Map.put(:arrival_time, departure_time)
         |> Map.put(:stop_position, from_stop_position)
 
+      %{"duration" => duration, "geometry" => geometry} =
+        Osrm.route(first_stop.stop_position, to_stop_position)
+
       last_stop =
         Map.new()
-        |> Map.put(:arrival_time, departure_time)
         |> Map.put(:stop_position, to_stop_position)
-
-      %{"duration" => duration} = Osrm.route(first_stop.stop_position, last_stop.stop_position)
-
-      last_stop =
-        Map.update!(last_stop, :arrival_time, fn start_time ->
-          Time.from_iso8601!(start_time) |> Time.add(trunc(duration)) |> Time.truncate(:second)
-        end)
+        |> Map.put_new(
+          :arrival_time,
+          Time.from_iso8601!(departure_time)
+          |> Time.add(trunc(duration))
+          |> Time.truncate(:second)
+        )
 
       %{
         id: id,
@@ -63,42 +82,17 @@ defmodule Digiby.Fardtjanst do
             get_meters_between_positions(last_stop.stop_position, query_to_position)
           ),
         stop_times: [first_stop, last_stop],
-        travel_time: duration
+        travel_time: duration,
+        geometry: Map.get(geometry, "coordinates")
       }
     end)
     |> Enum.filter(fn %{start_stop: departure} ->
       Time.compare(Time.from_iso8601!(departure.arrival_time), start_time) == :gt
     end)
-    |> Enum.filter(fn %{start_stop: departure, last_stop: destination} ->
-      {query_to_lng, query_to_lat} = query_to_position
-
-      before_duration =
-        Osrm.route([departure.stop_position, destination.stop_position]) |> Map.get("duration")
-
-      after_duration =
-        Osrm.route([
-          departure.stop_position,
-          %{"lat" => query_to_lat, "lng" => query_to_lng},
-          destination.stop_position
-        ])
-        |> Map.get("duration")
-
-      after_duration - before_duration < @max_extra_travel_time_for_fardtjanst
-    end)
     |> Enum.sort_by(
       fn %{start_stop: destination1} -> Time.from_iso8601!(destination1.arrival_time) end,
       Time
     )
-    |> Enum.map(fn %{start_stop: departure} = trip ->
-      Map.put_new_lazy(trip, :geometry, fn ->
-        {query_to_lng, query_to_lat} = query_to_position
-
-        Osrm.route(departure.stop_position, %{"lat" => query_to_lat, "lng" => query_to_lng})
-        |> IO.inspect()
-        |> Map.get("geometry")
-        |> Map.get("coordinates")
-      end)
-    end)
     |> Enum.map(&fardtjanst_to_transport_struct/1)
   end
 
