eb0afe872735d9e19b268f972d7512ae05f807e1
Kristoffer Larberg <mail@kristofferlarberg.se>
2022-08-31 15:10:13 +0200

    fix: bg color departures

--- frontend/src/components/Section.tsx
+++ frontend/src/components/Section.tsx
@@ -11,7 +11,7 @@ const Section = ({
     return <section className="h-full w-full bg-pm-black">{children}</section>;
   } else if (departures) {
     return (
-      <section className="mx-auto w-full max-w-screen-sm px-6 pt-6">
+      <section className="mx-auto min-h-screen w-full max-w-screen-sm px-6 pt-6">
         {children}
       </section>
     );
